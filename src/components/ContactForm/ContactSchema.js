597ef81a0cd403466ec754d477fb906679dbecee
Lily Pavlyshyna <pavlisinalilia869@gmail.com>
2025-05-27 22:23:09 +0300

    hw-3 done

--- src/components/ContactForm/ContactSchema.js
+++ src/components/ContactForm/ContactSchema.js
@@ -6,7 +6,6 @@ export const ContactSchema = Yup.object().shape({
     .max(30, "Max 30 chars")
     .required("This is a required field"),
   number: Yup.string()
-    .matches(/^\d+$/, "Only digits allowed")
     .min(7, "Min 7 chars")
     .max(12, "Max 12 chars")
     .required("This is a required field"),
